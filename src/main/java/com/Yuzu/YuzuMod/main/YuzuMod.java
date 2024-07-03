d20061ae5aeffa9d7c3c91449d68cf4baac897b6
yuzu-krs <yztimai@icloud.com>
2024-07-04 01:34:40 +0900

    Modの説明文を追加

---
+++ src/main/java/com/Yuzu/YuzuMod/main/YuzuMod.java
@@ -0,0 +1,13 @@
+package com.Yuzu.YuzuMod.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("yuzumod")
+public class YuzuMod {
+    public static final String MOD_ID="yuzumod";
+
+    public YuzuMod(){
+
+    }
+
+}
